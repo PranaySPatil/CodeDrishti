1761507971816b570e16dcc4665c01b111958dd4
Gyan Prakash <gykuma@Gyans-MBP.guest.corp.microsoft.com>
2019-07-24 19:57:31 +0530

    improve caching

--- gmail/gmail/background.js
+++ gmail/gmail/background.js
@@ -1,35 +1,35 @@
+var result = null;
+var currentTabUrl = null;
+
 function httpGet(url, tab)
 {
   if(url.indexOf("github.com")>-1)
   {
     fetch(url).then(r => r.text()).then(result => {
         // Result now contains the response text, do what you want...
-        chrome.tabs.sendMessage(tab.id, {"html": result});
+        this.result = result;
+        sendResponse(tab.id);
     })
   }
-  // $.get(url, function(data, s){
-  //   chrome.tabs.sendMessage(tab.id, {"html": data});
-  // });
-  //   var xmlHttp = new XMLHttpRequest();
-  //   xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-  //   xmlHttp.send( null );
-  //   return xmlHttp.responseText;
 }
 
-// chrome.tabs.getSelected(null, function(tab) {
-//   tabUrl = encodeURIComponent(tab.url);
-//   httpGet("http://localhost:3000/api?url="+tabUrl, tab)
-// });
+function sendResponse(tabId){
+  chrome.tabs.sendMessage(tabId, {"html": this.result});
+}
 
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete' && tab.active) {
-    
+    if(this.currentTabUrl == tab.url){
+      sendResponse(tab.id);
+      return;
+    }
+
+    this.currentTabUrl = tab.url;
     // do your things
     if(tab.url.split('/').length>=4) {
       
       tabUrl = encodeURIComponent(tab.url);
       httpGet("http://localhost:3000/api?url="+tabUrl+"&format=dot", tab);
     }
-
   }
 })
--- nodeServer/index.js
+++ nodeServer/index.js
@@ -5,39 +5,21 @@ const fs = require('fs')
 const path = require('path')
 const app = express()
 const port = 3000
-const repoCacheBasePath = "C:\\Users\\pranpati\\Documents\\Hack19\\nodeServer"
+const repoCacheBasePath = process.cwd();
 
 app.get('/api', (req, res) => {
     console.log("Prcoessing for ");
     console.log(req.query);
-    //cloneRepo(req.query.url, req.query.format, res);
     processRequest(req.query.url, req.query.format, res);
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
-// function cloneRepo(url, outputMode, res) {
-//     // url="https://github.com/PranaySPatil/vsts-rm-extensions"
-//     console.log(url)
-//     console.log(outputMode)
-//     var repoName = url.split('/')[4]
-//     let repoPath = path.join(repoCacheBasePath,repoName);
-//     if(!fs.existsSync(repoPath)) {
-//         console.log("Cloning repo")
-//         sg.clone(url+".git");
-//         console.log("Cloned repo successfully")
-//     }
-//     let dependencies = depcruise([path.join(repoCacheBasePath, repoName)], {
-//         exclude: "^node_modules|^.git",
-//         outputType: outputMode || "html"
-//     });
-
-//     console.log(dependencies.modules);
-//     res.send(dependencies.modules);
-// }
-
 function processRequest(url, outputMode, res){
     var repoName = url.split('/')[4]
+    if(repoName.indexOf('?')!= -1){
+        repoName = repoName.split('?')[0];
+    }
     let repoPath = path.join(repoCacheBasePath,repoName);
     let subFolderPath = '';
     if(url.split('/').length>5 && url.split('/')[5]=="tree"){
